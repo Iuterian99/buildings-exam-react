3014fd3a98a40903b313df219cc53b70bdfac608
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-03-07 00:27:00 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-danger">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
