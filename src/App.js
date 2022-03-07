80bd9f76b6fa3bb9243573ae93cd8169dd47a15a
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-03-07 21:32:09 +0500

    good

--- src/App.js
+++ src/App.js
@@ -1,11 +1,66 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+// import { Link } from "react-router-dom";
+
+// Components
+import TopHeader from "./components/navbar/navbar";
 
 function App() {
   return (
-    <div className="container">
-      <h1 className="text-danger">Hello World</h1>
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light p-0">
+        <TopHeader />
+      </nav>
+      <main className="main_part">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          cumque fuga quidem, vitae harum pariatur, nisi quia sed maxime
+          necessitatibus excepturi perferendis asperiores architecto repudiandae
+          assumenda laboriosam natus est. Cum in nobis voluptas ipsa explicabo
+          architecto dolorum illo, natus minima accusantium, illum rerum odio
+          expedita at eveniet alias aliquid recusandae. Et distinctio rerum
+          alias quis id officiis nulla autem. Quidem rerum quibusdam quaerat
+          illum sit officia molestias alias quasi at sunt, velit ex amet
+          consequuntur deleniti earum delectus totam distinctio sequi dolores.
+          Saepe aut quia voluptas qui assumenda quae, quod repellendus laborum a
+          neque nam quibusdam deleniti sequi ratione illum praesentium
+          accusantium, veniam ipsum deserunt vel ducimus officiis laudantium.
+          Est, fugiat, aliquid quod ut nisi eligendi delectus reprehenderit ipsa
+          facere officia eaque deleniti voluptatibus nihil nobis nesciunt sed a
+          numquam? Perferendis deserunt, accusamus praesentium odio dolores
+          blanditiis illum adipisci, pariatur temporibus obcaecati ea minus
+          maxime ex et, quisquam eveniet autem eaque repudiandae! Delectus, quia
+          rerum. Amet, eaque. Commodi necessitatibus asperiores eum quasi
+          laudantium perferendis sunt sapiente ad dicta, modi, accusantium id
+          fugiat atque architecto nostrum vel officia ut animi quas earum.
+          Soluta voluptates nulla consectetur saepe iure expedita sunt
+          necessitatibus et laboriosam quo hic, blanditiis quaerat id tempore
+          magni tenetur. Quisquam itaque, eum nulla recusandae temporibus sit
+          omnis asperiores, in architecto aut veritatis ullam doloribus
+          distinctio hic beatae nam ad. Officiis fugiat culpa alias aut eligendi
+          consequatur totam eaque, fugit dolor vitae ratione molestiae
+          consectetur expedita sunt facere possimus, quibusdam sequi asperiores,
+          assumenda dolorem. Blanditiis dolores quaerat architecto non neque
+          dicta beatae ut nisi sapiente, ipsam perspiciatis adipisci sed totam
+          rerum. A, perferendis veniam, tempora, excepturi illo dolor commodi
+          incidunt cupiditate nobis sunt deleniti tempore. Mollitia fuga, odio
+          vitae temporibus blanditiis distinctio, non accusamus molestias ipsum
+          placeat nam obcaecati dolorum vero numquam inventore fugiat itaque
+          suscipit, molestiae corporis illum velit? Explicabo consectetur
+          corrupti repudiandae molestias totam dolore fuga cumque aliquid, natus
+          at, expedita eaque, suscipit nulla ut eos praesentium dolor beatae
+          optio mollitia rem reprehenderit illo! Deserunt sequi maxime aut
+          explicabo sunt, veniam beatae non, voluptatem dicta libero minus porro
+          quo molestiae ducimus? Enim nesciunt rerum saepe recusandae possimus,
+          deleniti explicabo at iusto sunt pariatur voluptatem aliquam odit.
+          Iure, accusantium est! Aperiam cum consequatur itaque voluptates
+          neque. Maxime aspernatur, nam iusto iure temporibus fugit! Cupiditate
+          voluptatum ipsa cum saepe eveniet labore, iure nulla impedit doloribus
+          nobis iste temporibus necessitatibus delectus eum dicta magni beatae
+          et! Illo eum iure molestias placeat.
+        </p>
+      </main>
+    </>
   );
 }
 
---
+++ src/components/AllBuildings/allBuildings.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const allBuildings = () => {
+  return <div>allBuildings</div>;
+};
+
+export default allBuildings;
